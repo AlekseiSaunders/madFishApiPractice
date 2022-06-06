05a19397c9db9743816d27d523301fbf3ab2b537
Aleksei Saunders <alekseisaunders@gmail.com>
2022-06-06 15:57:44 -0600

    add code to separate input value and input field to allow resetting of field on submit

--- public/main.js
+++ public/main.js
@@ -3,9 +3,11 @@ const displayGenera = document.getElementById('fishGenera');
 const displaySpecies = document.getElementById('numberSpecies');
 const displayDescribed = document.getElementById('describedNumber');
 const displayLocation = document.getElementById('primaryLocation');
+const fishInput = document.getElementById('fishG');
 
-const makeReq = async () => {
-  const fishG = document.getElementById('fishG').value;
+const makeReq = async (event) => {
+  event.preventDefault();
+  const fishG = fishInput.value;
   console.log(`${fishG}`);
   const res = await fetch(`/api/${fishG}`);
   const data = await res.json();
@@ -13,6 +15,7 @@ const makeReq = async () => {
   displaySpecies.textContent = `Number of Species: ${data.numberOfSpecies}`;
   displayDescribed.textContent = `Number of Described Species to date: ${data.numberDescribedSpecies}`;
   displayLocation.textContent = `Primary Location on Madagascar: ${data.primaryLocation}`;
+  fishInput.value = '';
 };
 
 submit.addEventListener('click', makeReq);
